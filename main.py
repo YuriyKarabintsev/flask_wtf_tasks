fe9bf3f1c51683e893e89b3cf7dc463dedffd65f
YuriyKarabintsev <urakarabintsev@gmail.com>
2023-02-13 20:01:12 +0700

    Начало решения задания 2

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ def index():
     return render_template('base.html', title='Заготовка',
                            username=user)
 
+@app.route("/training/<prof>")
+def profession(prof):
+    prof = prof.lower()
+    print(prof)
+    return render_template("prof.html", prof=prof)
+
 
 if __name__ == "__main__":
     app.run(port=8080, host="127.0.0.1")
