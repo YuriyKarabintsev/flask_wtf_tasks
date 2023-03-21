34fd579092e9acbeaf13c8dacfb21874c7e45bc8
YuriyKarabintsev <urakarabintsev@gmail.com>
2023-03-21 15:54:38 +0700

    Задача 3 решена

--- work.py
+++ work.py
@@ -14,6 +14,15 @@ def profession(prof):
     print(prof)
     return render_template("prof.html", prof=prof)
 
+@app.route("/list_prof/<list_type>")
+def show_list(list_type):
+    professions = ["инженер-исследователь", "пилот", "строитель", "экзобиолог", "врач",
+                   "инженер по терраформированию", "климатолог", "специалист по радиационной защите",
+                   "астрогеолог", "гляциолог", "инженер жизнеобеспечения", "метеоролог",
+                   "оператор марсохода", "киберинженер", "штурман", "пилот дронов"]
+    return render_template("show_list.html", list_type=list_type, professions=professions)
+
+
 if __name__ == "__main__":
     app.run(port=8080, host="127.0.0.1")
 
