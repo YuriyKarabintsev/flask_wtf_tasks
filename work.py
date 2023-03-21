db5e10ce6c5bc0cabb5a28ce76c8916bf2d8eb97
YuriyKarabintsev <urakarabintsev@gmail.com>
2023-03-21 15:39:37 +0700

    Задача 2 решена

--- work.py
+++ work.py
@@ -2,23 +2,19 @@ import flask
 from flask import Flask
 from flask import render_template
 
-
 app = Flask(__name__)
 
-
 @app.route("/")
-@app.route("/index")
 def index():
-    user = "Ученик Яндекс.Лицея"
-    return render_template('base.html', title='Заготовка',
-                           username=user)
+    print("index")
+    return render_template('base.html', title='Заготовка')
 
 @app.route("/training/<prof>")
 def profession(prof):
-    prof = prof.lower()
     print(prof)
     return render_template("prof.html", prof=prof)
 
-
 if __name__ == "__main__":
     app.run(port=8080, host="127.0.0.1")
+
+print("works")
